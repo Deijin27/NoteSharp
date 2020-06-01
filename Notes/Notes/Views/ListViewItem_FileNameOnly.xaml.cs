e7b346a2f92b98aeb6bd167222c609eece3df3a2
Mia Boulter <miaboulter@outlook.com>
2020-06-01 17:23:47 +0100

    Moved some ListViewItems into ContentViews and reused them in NotesPage and NotesMovePage.

---
+++ Notes/Notes/Views/ListViewItem_FileNameOnly.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewItem_FileNameOnly : ContentView
+    {
+        public ListViewItem_FileNameOnly()
+        {
+            InitializeComponent();
+        }
+    }
+}
