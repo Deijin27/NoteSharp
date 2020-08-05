78c6968fbacb8047a1a24ab0b95fe7a749f6066a
Mia Boulter <miaboulter@outlook.com>
2020-08-05 13:15:17 +0100

    Changed behaviour of save button and close button in NoteEntryPage. Unfortunately it doesn't seem possible to visibly disable a toolbar item like I had planned.

--- Notes/Notes/Pages/NoteEntryPage.xaml.cs
+++ Notes/Notes/Pages/NoteEntryPage.xaml.cs
@@ -17,7 +17,11 @@ namespace Notes.Pages
         private string InitialText;
         private string InitialName;
         NotesPage PreviousPage;
-        //Note NoteStorage;
+
+        public Note CurrentNote { get; set; }
+
+        public bool UnsavedChangesExist { get; set; }
+
 
         /// <summary>
         /// Use when loading an existing note
@@ -35,7 +39,9 @@ namespace Notes.Pages
 
             //NoteStorage = note;
 
-            BindingContext = note;
+            CurrentNote = note;
+            BindingContext = this;
+            UnsavedChangesExist = false;
         }
 
         public void UnfocusAll() 
@@ -68,7 +74,9 @@ namespace Notes.Pages
             InitializeComponent();
             ApplySettings();
 
-            BindingContext = note;
+            CurrentNote = note;
+            BindingContext = this;
+            UnsavedChangesExist = false;
         }
 
         public void ApplySettings()
@@ -80,43 +88,21 @@ namespace Notes.Pages
 
         #region Close Clicked
 
-        async void Cancel_Clicked(object sender, EventArgs e)
+        async void Close_Clicked(object sender, EventArgs e)
         {
-            Note note = (Note)BindingContext;
+            Note note = CurrentNote;
 
-            #region OldStuff
-            /*if (note.Name != InitialName || note.Text != InitialText)
-            {
-                bool answer = await DisplayAlert
-                (
-                    AppResources.Alert_ExitWithoutSaving_Title, 
-                    AppResources.Alert_ExitWithoutSaving_Message, 
-                    AppResources.AlertOption_Yes, 
-                    AppResources.AlertOption_No
-                );
-                if (answer)
-                {
-                    await Navigation.PopModalAsync();
-                }
-            }
-            else
-            {
-                UnfocusAll();
-                await Navigation.PopModalAsync();
-            }*/
-            #endregion
-
-            if (note.Name != InitialName || note.Text != InitialText)
+            if (UnsavedChangesExist)
             {
                 var popup = new TwoOptionPopupPage
                 (
-                    "Close without saving?",
-                    "Any changes made will be lost.",
+                    "Save Changes?",
+                    "Unsaved changes exist, would you like to save them?",
                     "Yes",
-                    "Cancel"
+                    "No"
                 );
-                popup.LeftOptionClicked += ProceedWithClose;
-                popup.RightOptionClicked += CancelClose;
+                popup.LeftOptionClicked += SaveThenClose;
+                popup.RightOptionClicked += ProceedWithClose;
                 await PopupNavigation.Instance.PushAsync(popup);
             }
             else
@@ -129,36 +115,88 @@ namespace Notes.Pages
 
         async void ProceedWithClose()
         {
-            Note note = (Note)BindingContext;
-            note.Name = InitialName;
-            note.Text = InitialText;
+            
+            CurrentNote.Name = InitialName;
+            CurrentNote.Text = InitialText;
             UnfocusAll();
             await PopupNavigation.Instance.PopAsync();
             await Navigation.PopModalAsync();
         }
 
-        async void CancelClose()
+        async void SaveThenClose()
         {
             await PopupNavigation.Instance.PopAsync();
+            CloseAfter = true;
+            Save();
         }
 
         #endregion
 
         #region Save Clicked
-        async void OnSaveButtonClicked(object sender, EventArgs e)
+        void Save_Clicked(object sender, EventArgs e)
         {
-        Note note = (Note)BindingContext;
-        if (NewNote)
-        {
-            if (note.Name != InitialName)
+            if (UnsavedChangesExist)
             {
+                Save();
+            }
+        }
 
-                if (await App.Database.DoesNoteNameExistAsync(note.Name, note.FolderID, note.ID))
+        async void Save()
+        {
+            await DisplayAlert("Debug: Save Clicked", "", "Proceed");
+            Note note = CurrentNote;
+            if (NewNote)
+            {
+                if (note.Name != InitialName)
+                {
+
+                    if (await App.Database.DoesNoteNameExistAsync(note.Name, note.FolderID, note.ID))
+                    {
+                        var popup = new UniqueNamePromptPopupPage
+                        (
+                            "Note name conflict",
+                            "Input a different name for the note",
+                            "OK",
+                            "Cancel",
+                            note.FolderID,
+                            note.ID,
+                            App.Database.DoesNoteNameExistAsync,
+                            "Name invalid, it contains one of" + " \"/*.~",
+                            "Name conflict encountered; try a different name",
+                            "",
+                            "Input note name..."
+                        );
+                        popup.AcceptOptionClicked += ProceedRenameAndSaveNote;
+                        popup.CancelOptionClicked += CancelSaveNote;
+                        popup.BackgroundClicked += CancelSaveNote;
+                        popup.HardwareBackClicked += CancelSaveNote;
+
+                        await PopupNavigation.Instance.PushAsync(popup);
+                    }
+                    else
+                    {
+                        note.DateModified = DateTime.UtcNow;
+                        note.DateCreated = note.DateModified;
+                        await App.Database.SaveAsync(note);
+                        ChangesSaved?.Invoke();
+                        if (CloseAfter)
+                        {
+                            await Navigation.PopModalAsync();
+                        }
+                        else
+                        {
+                            InitialName = note.Name;
+                            InitialText = note.Text;
+                            UnsavedChangesExist = false;
+                        }
+                    }
+                }
+                else
                 {
                     var popup = new UniqueNamePromptPopupPage
                     (
-                        "Note name conflict",
-                        "Input a different name for the note",
+                        "Note name",
+                        "Input a name for the note",
                         "OK",
                         "Cancel",
                         note.FolderID,
@@ -176,91 +214,77 @@ namespace Notes.Pages
 
                     await PopupNavigation.Instance.PushAsync(popup);
                 }
+
+            }
+            else
+            {
+                UniqueNamePromptPopupPage.DoesNameExist namechecker;
+                if (note.IsQuickAccess) namechecker = App.Database.DoesQuickAccessOrOtherwiseNoteNameExistAsync;
+                else namechecker = App.Database.DoesNoteNameExistAsync;
+
+                if (note.Name != InitialName && (await namechecker.Invoke(note.Name, note.FolderID, note.ID)))
+                {
+                    var popup = new UniqueNamePromptPopupPage
+                    (
+                        "Note name conflict",
+                        "Input a different name for the note",
+                        "OK",
+                        "Cancel",
+                        note.FolderID,
+                        note.ID,
+                        namechecker,
+                        "Name invalid, it contains one of" + " \"/*.~",
+                        "Name conflict encountered; try a different name",
+                        "",
+                        "Input note name..."
+                    );
+                    popup.AcceptOptionClicked += ProceedRenameAndSaveNote;
+                    popup.CancelOptionClicked += CancelSaveNote;
+                    popup.BackgroundClicked += CancelSaveNote;
+                    popup.HardwareBackClicked += CancelSaveNote;
+
+                    await PopupNavigation.Instance.PushAsync(popup);
+                }
                 else
                 {
                     note.DateModified = DateTime.UtcNow;
-                    note.DateCreated = note.DateModified;
                     await App.Database.SaveAsync(note);
                     ChangesSaved?.Invoke();
-                    UnfocusAll();
-                    await Navigation.PopModalAsync();
+                    if (CloseAfter)
+                    {
+                        await Navigation.PopModalAsync();
+                    }
+                    else
+                    {
+                        InitialName = note.Name;
+                        InitialText = note.Text;
+                        UnsavedChangesExist = false;
+                    }
                 }
             }
-            else
-            {
-                var popup = new UniqueNamePromptPopupPage
-                (
-                    "Note name",
-                    "Input a name for the note",
-                    "OK",
-                    "Cancel",
-                    note.FolderID,
-                    note.ID,
-                    App.Database.DoesNoteNameExistAsync,
-                    "Name invalid, it contains one of" + " \"/*.~",
-                    "Name conflict encountered; try a different name",
-                    "",
-                    "Input note name..."
-                );
-                popup.AcceptOptionClicked += ProceedRenameAndSaveNote;
-                popup.CancelOptionClicked += CancelSaveNote;
-                popup.BackgroundClicked += CancelSaveNote;
-                popup.HardwareBackClicked += CancelSaveNote;
-
-                await PopupNavigation.Instance.PushAsync(popup);
-            }
-
-        }
-        else
-        {
-            UniqueNamePromptPopupPage.DoesNameExist namechecker;
-            if (note.IsQuickAccess) namechecker = App.Database.DoesQuickAccessOrOtherwiseNoteNameExistAsync;
-            else namechecker = App.Database.DoesNoteNameExistAsync;
-
-            if (note.Name != InitialName && (await namechecker.Invoke(note.Name, note.FolderID, note.ID)))
-            {
-                var popup = new UniqueNamePromptPopupPage
-                (
-                    "Note name conflict",
-                    "Input a different name for the note",
-                    "OK",
-                    "Cancel",
-                    note.FolderID,
-                    note.ID,
-                    namechecker,
-                    "Name invalid, it contains one of" + " \"/*.~",
-                    "Name conflict encountered; try a different name",
-                    "",
-                    "Input note name..."
-                );
-                popup.AcceptOptionClicked += ProceedRenameAndSaveNote;
-                popup.CancelOptionClicked += CancelSaveNote;
-                popup.BackgroundClicked += CancelSaveNote;
-                popup.HardwareBackClicked += CancelSaveNote;
-
-                await PopupNavigation.Instance.PushAsync(popup);
-            }
-            else
-            {
-                note.DateModified = DateTime.UtcNow;
-                await App.Database.SaveAsync(note);
-                ChangesSaved?.Invoke();
-                UnfocusAll();
-                await Navigation.PopModalAsync();
-            }
-        }
         }
 
+        bool CloseAfter = false;
         public async void ProceedRenameAndSaveNote(PromptPopupOptionEventArgs e)
         {
-            Note note = (Note)BindingContext;
+            Note note = CurrentNote;
             note.Name = e.Text;
             note.DateModified = DateTime.UtcNow;
             note.DateCreated = note.DateModified;
             await PopupNavigation.Instance.PopAsync();
             await App.Database.SaveAsync(note);
             ChangesSaved?.Invoke();
-            await Navigation.PopModalAsync();
+
+            if (CloseAfter)
+            {
+                await Navigation.PopModalAsync();
+            }
+            else
+            {
+                InitialName = note.Name;
+                InitialText = note.Text;
+                UnsavedChangesExist = false;
+            }
         }
 
         public async void CancelSaveNote(PromptPopupOptionEventArgs e)
@@ -274,23 +298,27 @@ namespace Notes.Pages
 
         async void HtmlPreview_Clicked(object sender, EventArgs e)
         {
-            var note = (Note)BindingContext;
-            string markdownText = note.Text;
-            await Navigation.PushAsync(new HtmlPreviewPage(markdownText, note.FolderID));
+            await Navigation.PushAsync(new HtmlPreviewPage(CurrentNote.Text, CurrentNote.FolderID));
         }
 
         async void MarkdownPreview_Clicked(object sender, EventArgs e)
         {
-            var note = (Note)BindingContext;
-            string markdownText = note.Text;
-            await Navigation.PushAsync(new MarkdownPreviewPage(markdownText, note.FolderID));
+            await Navigation.PushAsync(new MarkdownPreviewPage(CurrentNote.Text, CurrentNote.FolderID));
         }
 
         async void MarkdownView_Clicked(object sender, EventArgs e)
         {
-            var note = (Note)BindingContext;
-            string markdownText = note.Text;
-            await Navigation.PushAsync(new MarkdownViewPage(markdownText, note.FolderID));
+            await Navigation.PushAsync(new MarkdownViewPage(CurrentNote.Text, CurrentNote.FolderID));
+        }
+
+        private void NameEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UnsavedChangesExist = true;
+        }
+
+        private void MainEditor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UnsavedChangesExist = true;
         }
     }
 }
